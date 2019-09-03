6ecbc5da08b81c87296041bb8b39e9fdc79d64bb
Ubuntu <ubuntu@ip-172-31-24-130.ap-southeast-2.compute.internal>
2019-09-03 05:54:21 +0000

    Added new properties

--- aba/generator.py
+++ aba/generator.py
@@ -4,7 +4,7 @@ aba.generator
 """
 from __future__ import absolute_import
 
-from aba.records import *
+from .records import *
 
 
 class AbaFile(object):
@@ -13,8 +13,32 @@ class AbaFile(object):
     def __init__(self, header):
         self.header = header
         self.records = []
-        self.total_debit = 0
-        self.total_credit = 0
+
+    @property
+    def total_debit(self):
+        return self._calculate_total_debit()
+
+    @property
+    def total_credit(self):
+        return self._calculate_total_credit()
+
+    @property
+    def count(self):
+        return len(records)
+
+    def _calculate_total_debit(self):
+        result = 0
+        for record in self.records:
+            if int(record.fields[4].value) == 13:
+                result += int(record.fields[5].value)
+        return result
+
+    def _calculate_total_credit(self):
+        result = 0
+        for record in self.records:
+            if int(record.fields[4].value) in (50, 53):
+                result += int(record.fields[5].value)
+        return result
 
     def add_record(self, record):
         self.records.append(record)
@@ -23,16 +47,12 @@ class AbaFile(object):
         output = self.header.render_to_string() + line_ending
 
         for record in self.records:
-            if int(record.fields[4].value) in (50, 53):
-                self.total_credit += int(record.fields[5].value)
-            if int(record.fields[4].value) == 13:
-                self.total_debit += int(record.fields[5].value)
             output += record.render_to_string() + line_ending
 
         total = TotalRecord(
             total_credit=self.total_credit,
             total_debit=self.total_debit,
-            count=len(self.records)
+            count=self.count
         )
         output += total.render_to_string()
 
